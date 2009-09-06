d8fbee3afc6295bd28450f3160dbd1c535deb5b6
phil <phil@beadling.co.uk>
2009-09-06 16:22:23 +0000

    Mac Mini test - added comment

--- FFI/Octave/OctaveTest.m
+++ FFI/Octave/OctaveTest.m
@@ -1,4 +1,5 @@
 % Test our Haskell function in Octave
 hs_init()
+% Strike=100, Vol=20%, T=1, IR=5%, Time Steps=1, Sims=100000
 price_option(100,0.2,1,0.05,1,100000,'Halton','Box Muller','European')
 hs_exit()
