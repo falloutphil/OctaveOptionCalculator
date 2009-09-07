37abf23515d15a3bb2b89e0f90cc33ee44d489f5
phil <phil@beadling.co.uk>
2009-09-07 23:29:59 +0000

    More detail in Octave script

--- FFI/Octave/OctaveTest.m
+++ FFI/Octave/OctaveTest.m
@@ -2,14 +2,21 @@
 
 hs_init()
 
-T     = 1:-0.05:0;
-S0    = 30:70;
+T     = 2:-0.05:0;
+S0    = 20:80;
 K     = 50;
 sigma = 0.4;
 r     = 0.1;
+ts    = 1;
+sims  = 1000;
 [X,Y] = meshgrid(T,S0);
-f = @(time,price) price_option(price,K,sigma,time,r,1,1000,'Halton','Box Muller','European');
+f = @(time,price) \
+    price_option(price,K,sigma,time,r,ts,sims,'Halton','Box Muller','European');
 surf(X,Y,f(X,Y))
+title('Option Surface - Time to Expiry vs Initial Price vs Value')
+xlabel('Time to Expiry')
+ylabel('Initial Stock Price')
+zlabel('Value')
 % Need to pause whilst graph is shown or it will segv
 pause()
 
